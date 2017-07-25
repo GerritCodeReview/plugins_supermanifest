be3f2b75a9867a52229e235d1e8cea6d28fee9e4
Ankur Mittal <anmittal@google.com>
2017-07-25 16:32:01 -0700

    Abstract away update method to support more tools
    Abstract away update method inside an interface so that other tools
    similar to repo can be supported.
    
    Change-Id: I4964e0f9f89b4a02ce1cd7bb2cdcffb6cf24f585

--- src/main/java/com/googlesource/gerrit/plugins/supermanifest/SuperManifestRefUpdatedListener.java
+++ src/main/java/com/googlesource/gerrit/plugins/supermanifest/SuperManifestRefUpdatedListener.java
@@ -30,10 +30,10 @@ import com.google.gerrit.server.project.NoSuchProjectException;
 import com.google.gerrit.server.project.ProjectCache;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import java.io.ByteArrayInputStream;
 import java.io.Closeable;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -41,23 +41,18 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.InvalidRemoteException;
 import org.eclipse.jgit.api.errors.RefNotFoundException;
 import org.eclipse.jgit.errors.ConfigInvalidException;
 import org.eclipse.jgit.errors.RepositoryNotFoundException;
-import org.eclipse.jgit.errors.RevisionSyntaxException;
-import org.eclipse.jgit.gitrepo.ManifestParser;
 import org.eclipse.jgit.gitrepo.RepoCommand;
 import org.eclipse.jgit.lib.Config;
 import org.eclipse.jgit.lib.ObjectId;
-import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.lib.PersonIdent;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.revwalk.RevCommit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -66,10 +61,9 @@ import org.slf4j.LoggerFactory;
  * changes, it will trigger an update of the associated superproject.
  */
 @Singleton
-class SuperManifestRefUpdatedListener implements GitReferenceUpdatedListener, LifecycleListener {
+public class SuperManifestRefUpdatedListener
+    implements GitReferenceUpdatedListener, LifecycleListener {
   private static final Logger log = LoggerFactory.getLogger(SuperManifestRefUpdatedListener.class);
-
-  private static final String SECTION_NAME = "superproject";
 
   private final GitRepositoryManager repoManager;
   private final URI canonicalWebUrl;
@@ -119,127 +113,6 @@ class SuperManifestRefUpdatedListener implements GitReferenceUpdatedListener, Li
     log.info(canonicalWebUrl + " : " + String.format(formatStr, args));
   }
 
-  private static byte[] readBlob(Repository repo, String idStr) throws IOException {
-    try (ObjectReader reader = repo.newObjectReader()) {
-      ObjectId id = repo.resolve(idStr);
-      if (id == null) {
-        throw new RevisionSyntaxException(
-            String.format("repo %s does not have %s", repo.toString(), idStr), idStr);
-      }
-      return reader.open(id).getCachedBytes(Integer.MAX_VALUE);
-    }
-  }
-
-  private static class ConfigEntry {
-    Project.NameKey srcRepoKey;
-    String srcRef;
-    URI baseUri;
-    String xmlPath;
-    Project.NameKey destRepoKey;
-    boolean recordSubmoduleLabels;
-
-    // destBranch can be "*" in which case srcRef is ignored.
-    String destBranch;
-
-    ConfigEntry(Config cfg, String name) throws ConfigInvalidException {
-      String[] parts = name.split(":");
-      if (parts.length != 2) {
-        throw new ConfigInvalidException(
-            String.format("pluginName '%s' must have form REPO:BRANCH", name));
-      }
-
-      String destRepo = parts[0];
-      String destRef = parts[1];
-
-      if (!destRef.startsWith(REFS_HEADS)) {
-        throw new ConfigInvalidException(
-            String.format("invalid destination '%s'. Must specify refs/heads/", destRef));
-      }
-
-      if (destRef.contains("*") && !destRef.equals(REFS_HEADS + "*")) {
-        throw new ConfigInvalidException(
-            String.format("invalid destination '%s'. Use just '*' for all branches.", destRef));
-      }
-
-      String srcRepo = cfg.getString(SECTION_NAME, name, "srcRepo");
-      if (srcRepo == null) {
-        throw new ConfigInvalidException(String.format("entry %s did not specify srcRepo", name));
-      }
-
-      // TODO(hanwen): sanity check repo names.
-      srcRepoKey = new Project.NameKey(srcRepo);
-
-      if (destRef.equals(REFS_HEADS + "*")) {
-        srcRef = "";
-      } else {
-        if (!Repository.isValidRefName(destRef)) {
-          throw new ConfigInvalidException(
-              String.format("destination branch '%s' invalid", destRef));
-        }
-
-        srcRef = cfg.getString(SECTION_NAME, name, "srcRef");
-        if (!Repository.isValidRefName(srcRef)) {
-          throw new ConfigInvalidException(String.format("source ref '%s' invalid", srcRef));
-        }
-
-        if (srcRef == null) {
-          throw new ConfigInvalidException(String.format("entry %s did not specify srcRef", name));
-        }
-      }
-
-      xmlPath = cfg.getString(SECTION_NAME, name, "srcPath");
-      if (xmlPath == null) {
-        throw new ConfigInvalidException(String.format("entry %s did not specify srcPath", name));
-      }
-
-      destRepoKey = new Project.NameKey(destRepo);
-
-      // The external format is chosen so we can support copying over tags as well.
-      destBranch = destRef.substring(REFS_HEADS.length());
-
-      recordSubmoduleLabels = cfg.getBoolean(SECTION_NAME, name, "recordSubmoduleLabels", false);
-
-      try {
-        // http://foo/platform/manifest => http://foo/platform/
-        baseUri = new URI(srcRepoKey.toString()).resolve("");
-      } catch (URISyntaxException exception) {
-        throw new ConfigInvalidException("could not build src URL", exception);
-      }
-    }
-
-    public String src() {
-      String src = srcRef;
-      if (destBranch.equals("*")) {
-        src = "*";
-      }
-      return srcRepoKey + ":" + src + ":" + xmlPath;
-    }
-
-    public String dest() {
-      return destRepoKey + ":" + destBranch;
-    }
-
-    @Override
-    public String toString() {
-      return String.format("%s => %s", src(), dest());
-    }
-
-    @Override
-    public boolean equals(Object o) {
-      if (this == o) return true;
-      if (o == null || getClass() != o.getClass()) return false;
-
-      ConfigEntry that = (ConfigEntry) o;
-      if (!destRepoKey.equals(that.destRepoKey)) return false;
-      return destBranch.equals(that.destBranch);
-    }
-
-    @Override
-    public int hashCode() {
-      return Objects.hash(destRepoKey, destBranch);
-    }
-  }
-
   /*
      [superproject "submodules:refs/heads/nyc"]
         srcRepo = platforms/manifest
@@ -260,11 +133,11 @@ class SuperManifestRefUpdatedListener implements GitReferenceUpdatedListener, Li
     Set<String> sources = new HashSet<>();
 
     for (String sect : cfg.getSections()) {
-      if (!sect.equals(SECTION_NAME)) {
+      if (!sect.equals(ConfigEntry.SECTION_NAME)) {
         warn("%s.config: ignoring invalid section %s", name, sect);
       }
     }
-    for (String subsect : cfg.getSubsections(SECTION_NAME)) {
+    for (String subsect : cfg.getSubsections(ConfigEntry.SECTION_NAME)) {
       try {
         ConfigEntry configEntry = new ConfigEntry(cfg, subsect);
         if (destinations.contains(configEntry.srcRepoKey.get())
@@ -359,8 +232,13 @@ class SuperManifestRefUpdatedListener implements GitReferenceUpdatedListener, Li
       }
 
       try {
-        update(c, event.getRefName());
-      } catch (IOException | GitAPIException e) {
+        SubModuleUpdater subModuleUpdater = c.getSubModuleUpdater(serverIdent, canonicalWebUrl);
+        try (GerritRemoteReader reader = new GerritRemoteReader()) {
+          subModuleUpdater.update(reader, c, event.getRefName());
+        }
+      } catch (
+          Exception
+              e) { //catch all exceptions as gerrit doesn't print stack trace for thrown Exception
         // We only want the trace up to here. We could recurse into the exception, but this at least
         // trims the very common jgit.gitrepo.RepoCommand.RemoteUnavailableException.
         StackTraceElement here = Thread.currentThread().getStackTrace()[1];
@@ -370,7 +248,10 @@ class SuperManifestRefUpdatedListener implements GitReferenceUpdatedListener, Li
         // feedback to. We log the error, but it would be nice if we could surface these logs
         // somewhere.  Perhaps we could store these as commits in some special branch (but in
         // what repo?).
-        error("update for %s (ref %s) failed: %s", c.toString(), event.getRefName(), e);
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        e.printStackTrace(pw);
+        error("update for %s (ref %s) failed: %s", c.toString(), event.getRefName(), sw);
       }
     }
   }
@@ -393,57 +274,8 @@ class SuperManifestRefUpdatedListener implements GitReferenceUpdatedListener, Li
     return trimmed.toArray(new StackTraceElement[trimmed.size()]);
   }
 
-  private static class GerritIncludeReader implements ManifestParser.IncludedFileReader {
-    private final Repository repo;
-    private final String ref;
-
-    GerritIncludeReader(Repository repo, String ref) {
-      this.repo = repo;
-      this.ref = ref;
-    }
-
-    @Override
-    public InputStream readIncludeFile(String path) throws IOException {
-      String blobRef = ref + ":" + path;
-      return new ByteArrayInputStream(readBlob(repo, blobRef));
-    }
-  }
-
-  private void update(ConfigEntry c, String srcRef) throws IOException, GitAPIException {
-    try (GerritRemoteReader reader = new GerritRemoteReader()) {
-      Repository destRepo = reader.openRepository(c.destRepoKey.toString());
-      Repository srcRepo = reader.openRepository(c.srcRepoKey.toString());
-
-      RepoCommand cmd = new RepoCommand(destRepo);
-
-      if (c.destBranch.equals("*")) {
-        cmd.setTargetBranch(srcRef.substring(REFS_HEADS.length()));
-      } else {
-        cmd.setTargetBranch(c.destBranch);
-      }
-
-      InputStream manifestStream =
-          new ByteArrayInputStream(readBlob(srcRepo, srcRef + ":" + c.xmlPath));
-
-      cmd.setAuthor(serverIdent)
-          .setRecordRemoteBranch(true)
-          .setRecordSubmoduleLabels(c.recordSubmoduleLabels)
-          .setInputStream(manifestStream)
-          .setRecommendShallow(true)
-          .setRemoteReader(reader)
-          .setTargetURI(c.destRepoKey.toString())
-          .setURI(c.baseUri.toString());
-
-      // Must setup a included file reader; the default is to read the file from the filesystem
-      // otherwise, which would leak data from the serving machine.
-      cmd.setIncludedFileReader(new GerritIncludeReader(srcRepo, srcRef));
-
-      RevCommit commit = cmd.call();
-    }
-  }
-
-  // GerritRemoteReader is for injecting Gerrit's Git implementation into JGit.
-  private class GerritRemoteReader implements RepoCommand.RemoteReader, Closeable {
+  //GerritRemoteReader is for injecting Gerrit's Git implementation into JGit.
+  public class GerritRemoteReader implements RepoCommand.RemoteReader, Closeable {
     private final Map<String, Repository> repos;
 
     GerritRemoteReader() {
@@ -492,10 +324,10 @@ class SuperManifestRefUpdatedListener implements GitReferenceUpdatedListener, Li
         throws GitAPIException, IOException {
       Repository repo;
       repo = openRepository(repoName);
-      return readBlob(repo, ref + ":" + path);
+      return Utils.readBlob(repo, ref + ":" + path);
     }
 
-    private Repository openRepository(String name) throws IOException {
+    public Repository openRepository(String name) throws IOException {
       name = urlToRepoKey(canonicalWebUrl, name);
       if (repos.containsKey(name)) {
         return repos.get(name);

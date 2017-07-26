3955503d47f2d32baafe53f7c182229e64438488
Ankur Mittal <anmittal@google.com>
2017-07-26 11:37:37 -0700

    Add support for jiri manifest
    Change-Id: I8b1a2c63e472b15253939df15617ef362919a064

---
+++ src/main/java/com/googlesource/gerrit/plugins/supermanifest/JiriProjects.java
@@ -0,0 +1,187 @@
+// Copyright (C) 2017 Google Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.supermanifest;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+
+class JiriProjects {
+  @XmlElement(name = "project")
+  private Project[] projects;
+
+  public JiriProjects() {
+    projects = new Project[0];
+  }
+
+  /** @return the projects */
+  public Project[] getProjects() {
+    return projects;
+  }
+
+  public void sortByPath() {
+    Arrays.sort(projects, new SortbyPath());
+  }
+
+  public String toSubmodules() {
+    StringBuffer buf = new StringBuffer();
+    sortByPath();
+    for (Project p : projects) {
+      buf.append(p.toSubmodules());
+      buf.append("\n");
+    }
+    return buf.toString();
+  }
+
+  public JiriProjects(Project[] projects) {
+    this.projects = projects;
+  }
+
+  @Override
+  public String toString() {
+    StringBuffer buf = new StringBuffer();
+    if (projects.length > 0) {
+      buf.append("projects:\n");
+      for (Project p : projects) {
+        buf.append(StringUtil.addTab(p.toString()));
+      }
+    }
+    return buf.toString();
+  }
+
+  static class Project {
+    @XmlAttribute(required = true)
+    private String name;
+
+    @XmlAttribute(required = true)
+    private String path;
+
+    @XmlAttribute(required = true)
+    private String remote;
+
+    @XmlAttribute private String remotebranch;
+
+    @XmlAttribute private String revision;
+
+    @XmlAttribute private int historydepth;
+
+    /** @return the name */
+    public String getName() {
+      return name;
+    }
+
+    /** @return the path */
+    public String getPath() {
+      return path;
+    }
+
+    /** @return the remote */
+    public String getRemote() {
+      return remote;
+    }
+
+    /** @return the historydepth */
+    public int getHistorydepth() {
+      return historydepth;
+    }
+
+    public String getRef() {
+      if (!revision.isEmpty()) {
+        return revision;
+      }
+      return remotebranch;
+    }
+
+    public Project() {
+      name = "";
+      path = "";
+      remote = "";
+      remotebranch = "";
+      revision = "";
+      historydepth = 0;
+    }
+
+    public void fillDefault() {
+      if (remotebranch.isEmpty()) {
+        remotebranch = "master";
+      }
+    }
+
+    @Override
+    public String toString() {
+      return String.format(
+          "project:\n\tname: %s\n\tpath: %s\n\tremote: %s\n\tremotebranch: %s\n\trevision: %s",
+          name, path, remote, remotebranch, revision);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      Project p = (Project) obj;
+
+      if (!name.equals(p.name)) {
+        return false;
+      }
+      if (!path.equals(p.path)) {
+        return false;
+      }
+      if (!remote.equals(p.remote)) {
+        return false;
+      }
+      if (!remotebranch.equals(p.remotebranch)) {
+        return false;
+      }
+      if (!revision.equals(p.revision)) {
+        return false;
+      }
+
+      return true;
+    }
+
+    public String toSubmodules() {
+      StringBuffer buf = new StringBuffer(String.format("[submodule \"%s\"]", name));
+      buf.append("\n\tpath = " + path);
+      buf.append("\n\turl = " + remote);
+      String branch = "";
+      if (!remotebranch.isEmpty()) {
+        branch = remotebranch;
+      }
+
+      if (!revision.isEmpty()) {
+        branch = revision;
+      }
+
+      if (!branch.isEmpty()) {
+        buf.append("\n\tbranch = " + branch);
+      }
+
+      buf.append("\n");
+      return buf.toString();
+    }
+
+    public String Key() {
+      return name + "=" + remote;
+    }
+  }
+
+  static class SortbyPath implements Comparator<Project> {
+    @Override
+    public int compare(Project a, Project b) {
+      String p1 = StringUtil.stripAndaddCharsAtEnd(a.getPath(), "/");
+      String p2 = StringUtil.stripAndaddCharsAtEnd(b.getPath(), "/");
+      return p1.compareTo(p2);
+    }
+  }
+}
---
+++ src/test/java/com/googlesource/gerrit/plugins/supermanifest/JiriSuperManifestIT.java
@@ -0,0 +1,391 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.supermanifest;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.fail;
+
+import com.google.gerrit.acceptance.GitUtil;
+import com.google.gerrit.acceptance.LightweightPluginDaemonTest;
+import com.google.gerrit.acceptance.PushOneCommit;
+import com.google.gerrit.acceptance.TestPlugin;
+import com.google.gerrit.extensions.api.projects.BranchApi;
+import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
+import com.google.gerrit.reviewdb.client.Project.NameKey;
+import com.google.gerrit.reviewdb.client.RefNames;
+import java.util.Arrays;
+import org.eclipse.jgit.internal.storage.dfs.InMemoryRepository;
+import org.eclipse.jgit.junit.TestRepository;
+import org.junit.Test;
+
+@TestPlugin(
+  name = "supermanifest",
+  sysModule = "com.googlesource.gerrit.plugins.supermanifest.SuperManifestModule"
+)
+public class JiriSuperManifestIT extends LightweightPluginDaemonTest {
+  NameKey[] testRepoKeys;
+
+  void setupTestRepos(String prefix) throws Exception {
+    testRepoKeys = new NameKey[2];
+    for (int i = 0; i < 2; i++) {
+      testRepoKeys[i] = createProject(prefix + i);
+
+      TestRepository<InMemoryRepository> repo = cloneProject(testRepoKeys[i], admin);
+
+      PushOneCommit push =
+          pushFactory.create(db, admin.getIdent(), repo, "Subject", "file" + i, "file");
+      push.to("refs/heads/master").assertOkStatus();
+    }
+  }
+
+  void pushConfig(String config) throws Exception {
+    // This will trigger a configuration reload.
+    TestRepository<InMemoryRepository> allProjectRepo = cloneProject(allProjects, admin);
+    GitUtil.fetch(allProjectRepo, RefNames.REFS_CONFIG + ":config");
+    allProjectRepo.reset("config");
+    PushOneCommit push =
+        pushFactory.create(
+            db, admin.getIdent(), allProjectRepo, "Subject", "supermanifest.config", config);
+    PushOneCommit.Result res = push.to("refs/meta/config");
+    res.assertOkStatus();
+  }
+
+  @Test
+  public void basicFunctionalityWorks() throws Exception {
+    setupTestRepos("project");
+
+    // Make sure the manifest exists so the configuration loads successfully.
+    NameKey manifestKey = createProject("manifest");
+    TestRepository<InMemoryRepository> manifestRepo = cloneProject(manifestKey, admin);
+
+    NameKey superKey = createProject("superproject");
+    cloneProject(superKey, admin);
+
+    pushConfig(
+        "[superproject \""
+            + superKey.get()
+            + ":refs/heads/destbranch\"]\n"
+            + "  srcRepo = "
+            + manifestKey.get()
+            + "\n"
+            + "  srcRef = refs/heads/srcbranch\n"
+            + "  srcPath = default\n"
+            + "  toolType = jiri\n");
+
+    // XML change will trigger commit to superproject.
+    String xml =
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+            + "<manifest>\n<projects>\n"
+            + "<project name=\""
+            + testRepoKeys[0].get()
+            + "\" remote=\""
+            + canonicalWebUrl.get()
+            + testRepoKeys[0].get()
+            + "\" path=\"project1\" />\n"
+            + "</projects>\n</manifest>\n";
+
+    pushFactory
+        .create(db, admin.getIdent(), manifestRepo, "Subject", "default", xml)
+        .to("refs/heads/srcbranch")
+        .assertOkStatus();
+
+    BranchApi branch = gApi.projects().name(superKey.get()).branch("refs/heads/destbranch");
+    assertThat(branch.file("project1").getContentType()).isEqualTo("x-git/gitlink; charset=UTF-8");
+    try {
+      branch.file("project2");
+      fail("wanted exception");
+    } catch (ResourceNotFoundException e) {
+      // all fine.
+    }
+
+    xml =
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+            + "<manifest>\n<projects>\n"
+            + "  <project name=\""
+            + testRepoKeys[0].get()
+            + "\" remote=\""
+            + canonicalWebUrl.get()
+            + testRepoKeys[0].get()
+            + "\" path=\"project1\" />\n"
+            + "  <project name=\""
+            + testRepoKeys[1].get()
+            + "\" remote=\""
+            + canonicalWebUrl.get()
+            + testRepoKeys[1].get()
+            + "\" path=\"project2\" />\n"
+            + "</projects>\n</manifest>\n";
+
+    pushFactory
+        .create(db, admin.getIdent(), manifestRepo, "Subject", "default", xml)
+        .to("refs/heads/srcbranch")
+        .assertOkStatus();
+
+    branch = gApi.projects().name(superKey.get()).branch("refs/heads/destbranch");
+    assertThat(branch.file("project2").getContentType()).isEqualTo("x-git/gitlink; charset=UTF-8");
+
+    // Make sure config change gets picked up.
+    pushConfig(
+        "[superproject \""
+            + superKey.get()
+            + ":refs/heads/other\"]\n"
+            + "  srcRepo = "
+            + manifestKey.get()
+            + "\n"
+            + "  srcRef = refs/heads/srcbranch\n"
+            + "  srcPath = default\n"
+            + "  toolType = jiri\n");
+
+    // Push another XML change; this should trigger a commit using the new config.
+    xml =
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+            + "<manifest>\n<projects>\n"
+            + "  <project name=\""
+            + testRepoKeys[1].get()
+            + "\" remote=\""
+            + canonicalWebUrl.get()
+            + testRepoKeys[1].get()
+            + "\" path=\"project3\" />\n"
+            + "</projects>\n</manifest>\n";
+
+    pushFactory
+        .create(db, admin.getIdent(), manifestRepo, "Subject", "default", xml)
+        .to("refs/heads/srcbranch")
+        .assertOkStatus();
+
+    branch = gApi.projects().name(superKey.get()).branch("refs/heads/other");
+    assertThat(branch.file("project3").getContentType()).isEqualTo("x-git/gitlink; charset=UTF-8");
+  }
+
+  private void outer() {
+    inner();
+  }
+
+  private void inner() {
+    throw new IllegalStateException();
+  }
+
+  private void innerTest() {
+    try {
+      outer();
+      fail("should throw");
+    } catch (IllegalStateException e) {
+      StackTraceElement[] trimmed =
+          SuperManifestRefUpdatedListener.trimStack(
+              e.getStackTrace(), Thread.currentThread().getStackTrace()[1]);
+      String str = Arrays.toString(trimmed);
+      assertThat(str).doesNotContain("trimStackTrace");
+      assertThat(str).contains("innerTest");
+    }
+  }
+
+  @Test
+  public void trimStackTrace() throws Exception {
+    innerTest();
+  }
+
+  @Test
+  public void wildcardDestBranchWorks() throws Exception {
+    setupTestRepos("project");
+
+    // Make sure the manifest exists so the configuration loads successfully.
+    NameKey manifestKey = createProject("manifest");
+    TestRepository<InMemoryRepository> manifestRepo = cloneProject(manifestKey, admin);
+
+    NameKey superKey = createProject("superproject");
+    cloneProject(superKey, admin);
+
+    pushConfig(
+        "[superproject \""
+            + superKey.get()
+            + ":refs/heads/*\"]\n"
+            + "  srcRepo = "
+            + manifestKey.get()
+            + "\n"
+            + "  srcRef = blablabla\n"
+            + "  srcPath = default\n"
+            + "  toolType = jiri\n");
+
+    // XML change will trigger commit to superproject.
+    String xml =
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+            + "<manifest>\n<projects>\n"
+            + "  <project name=\""
+            + testRepoKeys[0].get()
+            + "\" remote=\""
+            + canonicalWebUrl.get()
+            + testRepoKeys[0].get()
+            + "\" path=\"project1\" />\n"
+            + "</projects>\n</manifest>\n";
+
+    pushFactory
+        .create(db, admin.getIdent(), manifestRepo, "Subject", "default", xml)
+        .to("refs/heads/src1")
+        .assertOkStatus();
+
+    xml =
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+            + "<manifest>\n<projects>\n"
+            + "  <project name=\""
+            + testRepoKeys[1].get()
+            + "\" remote=\""
+            + canonicalWebUrl.get()
+            + testRepoKeys[1].get()
+            + "\" path=\"project2\" />\n"
+            + "</projects>\n</manifest>\n";
+
+    pushFactory
+        .create(db, admin.getIdent(), manifestRepo, "Subject", "default", xml)
+        .to("refs/heads/src2")
+        .assertOkStatus();
+
+    BranchApi branch1 = gApi.projects().name(superKey.get()).branch("refs/heads/src1");
+    assertThat(branch1.file("project1").getContentType()).isEqualTo("x-git/gitlink; charset=UTF-8");
+    try {
+      branch1.file("project2");
+      fail("wanted exception");
+    } catch (ResourceNotFoundException e) {
+      // all fine.
+    }
+
+    BranchApi branch2 = gApi.projects().name(superKey.get()).branch("refs/heads/src2");
+    assertThat(branch2.file("project2").getContentType()).isEqualTo("x-git/gitlink; charset=UTF-8");
+    try {
+      branch2.file("project1");
+      fail("wanted exception");
+    } catch (ResourceNotFoundException e) {
+      // all fine.
+    }
+  }
+
+  @Test
+  public void manifestIncludesOtherManifest() throws Exception {
+    setupTestRepos("project");
+
+    String xml =
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+            + "<manifest>\n<projects>\n"
+            + "<project name=\""
+            + testRepoKeys[0].get()
+            + "\" remote=\""
+            + canonicalWebUrl.get()
+            + testRepoKeys[0].get()
+            + "\" path=\"project1\" />\n"
+            + "</projects>\n</manifest>\n";
+
+    NameKey manifestKey = createProject("manifest");
+    TestRepository<InMemoryRepository> manifestRepo = cloneProject(manifestKey, admin);
+    pushFactory
+        .create(db, admin.getIdent(), manifestRepo, "Subject", "default", xml)
+        .to("refs/heads/master")
+        .assertOkStatus();
+
+    String superXml =
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+            + "<manifest>\n<imports>\n"
+            + "  <localimport file=\"default\"/>"
+            + "</imports>\n</manifest>";
+
+    pushFactory
+        .create(db, admin.getIdent(), manifestRepo, "Subject", "super", superXml)
+        .to("refs/heads/master")
+        .assertOkStatus();
+
+    NameKey superKey = createProject("superproject");
+    cloneProject(superKey, admin);
+
+    pushConfig(
+        "[superproject \""
+            + superKey.get()
+            + ":refs/heads/master\"]\n"
+            + "  srcRepo = "
+            + manifestKey.get()
+            + "\n"
+            + "  srcRef = refs/heads/master\n"
+            + "  srcPath = super\n"
+            + "  toolType = jiri\n");
+
+    // Push a change to the source branch. We intentionally change the included XML file
+    // (rather than the one mentioned in srcPath), to double check that we don't try to be too
+    // smart about eluding nops.
+    pushFactory
+        .create(db, admin.getIdent(), manifestRepo, "Subject", "default", xml + " ")
+        .to("refs/heads/master")
+        .assertOkStatus();
+
+    BranchApi branch = gApi.projects().name(superKey.get()).branch("refs/heads/master");
+    assertThat(branch.file("project1").getContentType()).isEqualTo("x-git/gitlink; charset=UTF-8");
+  }
+
+  @Test
+  public void remoteImportFails() throws Exception {
+    setupTestRepos("project");
+
+    String xml =
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+            + "<manifest>\n<projects>\n"
+            + "<project name=\""
+            + testRepoKeys[0].get()
+            + "\" remote=\""
+            + canonicalWebUrl.get()
+            + testRepoKeys[0].get()
+            + "\" path=\"project1\" />\n"
+            + "</projects>\n</manifest>\n";
+
+    NameKey manifestKey = createProject("manifest");
+    NameKey superKey = createProject("superproject");
+
+    cloneProject(superKey, admin);
+
+    pushConfig(
+        "[superproject \""
+            + superKey.get()
+            + ":refs/heads/master\"]\n"
+            + "  srcRepo = "
+            + manifestKey.get()
+            + "\n"
+            + "  srcRef = refs/heads/master\n"
+            + "  srcPath = super\n"
+            + "  toolType = jiri\n");
+    TestRepository<InMemoryRepository> manifestRepo = cloneProject(manifestKey, admin);
+    pushFactory
+        .create(db, admin.getIdent(), manifestRepo, "Subject", "default", xml)
+        .to("refs/heads/master")
+        .assertOkStatus();
+
+    String superXml =
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+            + "<manifest>\n<imports>\n"
+            + "<import manifest=\""
+            + manifestKey.get()
+            + "\" name=\"default\" remote=\""
+            + canonicalWebUrl.get()
+            + manifestKey.get()
+            + "\"/>"
+            + "</imports>\n</manifest>";
+
+    pushFactory
+        .create(db, admin.getIdent(), manifestRepo, "Subject", "super", superXml)
+        .to("refs/heads/master")
+        .assertOkStatus();
+
+    BranchApi branch = gApi.projects().name(superKey.get()).branch("refs/heads/master");
+    try {
+      branch.file("project1");
+      fail("wanted exception");
+    } catch (ResourceNotFoundException e) {
+      // all fine.
+    }
+  }
+}

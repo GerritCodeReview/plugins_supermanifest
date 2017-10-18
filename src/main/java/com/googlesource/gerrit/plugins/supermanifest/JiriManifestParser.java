499f87f8ccb42b74eba7de22ad850726514ebe82
Ankur Mittal <anmittal@google.com>
2017-10-18 14:29:47 -0700

    Support import tag in jiri manifest
    Change-Id: I73b0d87300824698d9f96853956d8b4c40a46387

--- src/main/java/com/googlesource/gerrit/plugins/supermanifest/JiriManifestParser.java
+++ src/main/java/com/googlesource/gerrit/plugins/supermanifest/JiriManifestParser.java
@@ -14,6 +14,10 @@
 
 package com.googlesource.gerrit.plugins.supermanifest;
 
+import static com.google.gerrit.reviewdb.client.RefNames.REFS_HEADS;
+
+import com.google.gerrit.reviewdb.client.Project;
+import com.googlesource.gerrit.plugins.supermanifest.SuperManifestRefUpdatedListener.GerritRemoteReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.net.URI;
@@ -28,43 +32,78 @@ import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import javax.xml.transform.stream.StreamSource;
+import org.apache.commons.lang.StringUtils;
 import org.eclipse.jgit.errors.ConfigInvalidException;
 import org.eclipse.jgit.lib.Repository;
 
 class JiriManifestParser {
-  public static JiriProjects getProjects(Repository repo, String ref, String manifest)
+  static class Work {
+    public Work(String repoKey, String manifest, String ref, String pKey, boolean revisionPinned) {
+      this.repoKey = repoKey;
+      this.manifest = manifest;
+      this.ref = ref;
+      this.revisionPinned = revisionPinned;
+      this.pKey = pKey;
+    }
+
+    String repoKey;
+    String manifest;
+    String ref;
+    boolean revisionPinned;
+
+    // In jiri if import is pinned to a revision and
+    // we have a corresponding project, jiri would like to
+    // pin that project to same revision. So passing key to match
+    // project to import tag.
+    String pKey;
+  }
+
+  public static JiriProjects getProjects(
+      GerritRemoteReader reader, String repoKey, String ref, String manifest)
       throws ConfigInvalidException, IOException {
-    Queue<String> q = new LinkedList<>();
-    q.add(manifest);
-    HashSet<String> processedFiles = new HashSet<>();
+
+    HashMap<String, Repository> repoMap = new HashMap<>();
+    repoMap.put(repoKey, reader.openRepository(repoKey));
+    Queue<Work> q = new LinkedList<>();
+    q.add(new Work(repoKey, manifest, ref, "", false));
+    HashMap<String, HashSet<String>> processedRepoFiles = new HashMap<>();
     HashMap<String, JiriProjects.Project> projectMap = new HashMap<>();
 
     while (q.size() != 0) {
-      String file = q.remove();
-      if (processedFiles.contains(file)) {
+      Work w = q.remove();
+      Repository repo = repoMap.get(w.repoKey);
+      if (repo == null) {
+        repo = reader.openRepository(w.repoKey);
+        repoMap.put(w.repoKey, repo);
+      }
+      HashSet<String> processedFiles = processedRepoFiles.get(w.repoKey);
+      if (processedFiles == null) {
+        processedFiles = new HashSet<String>();
+        processedRepoFiles.put(w.repoKey, processedFiles);
+      }
+      if (processedFiles.contains(w.manifest)) {
         continue;
       }
-      processedFiles.add(file);
+      processedFiles.add(w.manifest);
       JiriManifest m;
       try {
-        m = parseManifest(repo, ref, file);
+        m = parseManifest(repo, w.ref, w.manifest);
       } catch (JAXBException | XMLStreamException e) {
         throw new ConfigInvalidException("XML parse error", e);
-      }
-      if (m.imports.getImports().length != 0) {
-        throw new ConfigInvalidException(
-            String.format("Manifest %s contains remote imports which are not supported", file));
       }
 
       for (JiriProjects.Project project : m.projects.getProjects()) {
         project.fillDefault();
+        if (w.revisionPinned && project.Key().equals(w.pKey)) {
+          project.setRevision(w.ref);
+        }
         if (projectMap.containsKey(project.Key())) {
           if (!projectMap.get(project.Key()).equals(project))
             throw new ConfigInvalidException(
                 String.format(
                     "Duplicate conflicting project %s in manifest %s\n%s\n%s",
                     project.Key(),
-                    file,
+                    w.manifest,
                     project.toString(),
                     projectMap.get(project.Key()).toString()));
         } else {
@@ -74,12 +113,39 @@ class JiriManifestParser {
 
       URI parentURI;
       try {
-        parentURI = new URI(file);
+        parentURI = new URI(w.manifest);
       } catch (URISyntaxException e) {
         throw new ConfigInvalidException("Invalid parent URI", e);
       }
       for (JiriManifest.LocalImport l : m.imports.getLocalImports()) {
-        q.add(parentURI.resolve(l.getFile()).getPath());
+        Work tw =
+            new Work(
+                w.repoKey,
+                parentURI.resolve(l.getFile()).getPath(),
+                w.ref,
+                w.pKey,
+                w.revisionPinned);
+        q.add(tw);
+      }
+
+      for (JiriManifest.Import i : m.imports.getImports()) {
+        i.fillDefault();
+        URI uri;
+        try {
+          uri = new URI(i.getRemote());
+        } catch (URISyntaxException e) {
+          throw new ConfigInvalidException("Invalid URI", e);
+        }
+        String iRepoKey = new Project.NameKey(StringUtils.strip(uri.getPath(), "/")).toString();
+        String iRef = i.getRevision();
+        boolean revisionPinned = true;
+        if (iRef.isEmpty()) {
+          iRef = REFS_HEADS + i.getRemotebranch();
+          revisionPinned = false;
+        }
+
+        Work tw = new Work(iRepoKey, i.getManifest(), iRef, i.Key(), revisionPinned);
+        q.add(tw);
       }
     }
     return new JiriProjects(projectMap.values().toArray(new JiriProjects.Project[0]));

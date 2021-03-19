0c53720babe4fd096a15e8b39b421f43b09ba699
Ivan Frade <ifrade@google.com>
2021-03-19 12:06:23 -0700

    Normalize repo opening paths
    openRepository() doesn't know if it is receiving a URL or a repoName
    (the host-relative name of the project). It just tries to clean up the
    canonicalWebUrl and assumes it is a repoName. This is wrong for e.g.
    external URLs.
    
    Besides, the URL->repoName translation is inconsistent: sha1() just
    takes the path of the incoming URL (and calls openRepository()) while
    readFileWithMode passes through the URL. An external URL can open the
    wrong repo in the first case and nothing on the second.
    
    Make the caller explicit if it is sending a URI or a repoName. For plain
    openRepository() assume it is repoName. It seems all callers use
    repoName (otherwise we need to add some guessing code).
    
    Translate URIs to repoNames consistently (even if wrongly).
    
    This paves the way to plug a cross-host supporting implementation.
    
    Change-Id: Ia2a925764bb9c3305a434240d44873b31c6e0c75

---
+++ javatests/com/googlesource/gerrit/plugins/supermanifest/GerritRemoteReaderTest.java
@@ -0,0 +1,98 @@
+package com.googlesource.gerrit.plugins.supermanifest;
+
+import com.google.gerrit.entities.Project;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.gerrit.testing.InMemoryRepositoryManager;
+import org.eclipse.jgit.errors.RepositoryNotFoundException;
+import org.eclipse.jgit.gitrepo.RepoCommand;
+import org.eclipse.jgit.junit.TestRepository;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.junit.Test;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.gerrit.testing.GerritJUnit.assertThrows;
+
+public class GerritRemoteReaderTest {
+
+  private static final String MASTER = "refs/heads/master";
+  private static final String CANONICAL_WEB_URL = "https://example.com/gerrit";
+
+  GitRepositoryManager repoManager = new InMemoryRepositoryManager();
+  SuperManifestRefUpdatedListener.GerritRemoteReader reader =
+      new SuperManifestRefUpdatedListener.GerritRemoteReader(
+          repoManager, CANONICAL_WEB_URL);
+
+  @Test
+  public void sha1_inCanonical_repoNameWithoutCanonicalPrefix() throws Exception {
+    repoManager.createRepository(Project.nameKey("project/x"));
+    Repository repository = repoManager.openRepository(Project.nameKey("project/x"));
+    RevCommit projectXTip;
+    try (TestRepository<Repository> git = new TestRepository<>(repository)) {
+      projectXTip = git.branch(MASTER).commit().add("x", "xxx").create();
+    }
+
+    ObjectId objectId = reader.sha1(CANONICAL_WEB_URL + "/project/x", "refs/heads/master");
+    assertThat(objectId).isEqualTo(projectXTip);
+  }
+
+  @Test
+  public void sha1_outterHost_repoNameFromPath() throws Exception {
+    repoManager.createRepository(Project.nameKey("gerrit/project/x"));
+    Repository repository = repoManager.openRepository(Project.nameKey("gerrit/project/x"));
+    RevCommit projectXTip;
+    try (TestRepository<Repository> git = new TestRepository<>(repository)) {
+      projectXTip = git.branch(MASTER).commit().add("x", "xxx").create();
+    }
+
+    // This tries to open "gerrit/project/x" which (unfortunately) works
+    ObjectId objectId = reader.sha1("https://other-host.com/gerrit/project/x", "refs/heads/master");
+    assertThat(objectId).isEqualTo(projectXTip);
+  }
+
+  @Test
+  public void sha1_forObjectId_returnIt() throws Exception {
+    String sha1 = "91f2c8cb366e21c20544f531be710fdfa5eb3afb";
+    ObjectId oid = ObjectId.fromString(sha1);
+    assertThat(reader.sha1("http://this.is.not.read/", sha1)).isEqualTo(oid);
+  }
+
+  @Test
+  public void readWithFileMode_readsContents() throws Exception {
+    repoManager.createRepository(Project.nameKey("project/x"));
+    Repository repository = repoManager.openRepository(Project.nameKey("project/x"));
+    try (TestRepository<Repository> git = new TestRepository<>(repository)) {
+      git.branch(MASTER).commit().add("x", "contents").create();
+    }
+
+    // This tries to open "gerrit/project/x" which (unfortunately) works
+    RepoCommand.RemoteFile remoteFile = reader.readFileWithMode(CANONICAL_WEB_URL + "/project/x", "refs/heads/master", "x");
+    assertThat(new String(remoteFile.getContents())).isEqualTo("contents");
+  }
+
+  @Test
+  public void openRepository_byRepoName_ok() throws Exception {
+    repoManager.createRepository(Project.nameKey("project/x"));
+    Repository repository = repoManager.openRepository(Project.nameKey("project/x"));
+    try (TestRepository<Repository> git = new TestRepository<>(repository)) {
+      git.branch(MASTER).commit().add("x", "contents").create();
+    }
+
+    Repository repo = reader.openRepository("project/x");
+    assertThat(repo).isNotNull();
+  }
+
+  @Test
+  public void openRepository_byUri_doesNotOpen() throws Exception {
+    repoManager.createRepository(Project.nameKey("project/x"));
+    Repository repository = repoManager.openRepository(Project.nameKey("project/x"));
+    try (TestRepository<Repository> git = new TestRepository<>(repository)) {
+      git.branch(MASTER).commit().add("x", "contents").create();
+    }
+
+    // This tries to open "gerrit/project/x" which (unfortunately) works
+    assertThrows(RepositoryNotFoundException.class,
+        () -> reader.openRepository(CANONICAL_WEB_URL + "/project/x"));
+  }
+}

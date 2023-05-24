436f78a04964118e4d8c51f42714088fc60c351f
Ivan Frade <ifrade@google.com>
2023-05-24 10:32:49 -0700

    ConfigEntry: support glob destination branches
    Conf only allows a 1:1 mapping or a *:* mapping between manifest and
    superproject branches. In setups with one manifest repo feeding
    multiple superproject, this produces long and error-prone
    configurations, with many entries and long exclude lists.
    
    Support glob destinations in the conf, e.g.
    [superproject "superproject:refs/heads/*-release"]
    or
    [superproject "superproject:refs/heads/a-*-dev"]
    
    Change-Id: Ia1cec87f6db221b7a321941e59421c2972714c56

--- javatests/com/googlesource/gerrit/plugins/supermanifest/RepoSuperManifestIT.java
+++ javatests/com/googlesource/gerrit/plugins/supermanifest/RepoSuperManifestIT.java
@@ -27,6 +27,7 @@ import com.google.gerrit.acceptance.RestResponse;
 import com.google.gerrit.acceptance.TestPlugin;
 import com.google.gerrit.acceptance.testsuite.project.ProjectOperations;
 import com.google.gerrit.entities.Project;
+import com.google.gerrit.entities.Project.NameKey;
 import com.google.gerrit.entities.RefNames;
 import com.google.gerrit.extensions.api.projects.BranchApi;
 import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
@@ -489,6 +490,71 @@ public class RepoSuperManifestIT extends LightweightPluginDaemonTest {
   }
 
   @Test
+  public void regexDestBranchWorks() throws Exception {
+    setupTestRepos("project");
+
+    pushConfig(
+        "[superproject \""
+            + superKey.get()
+            + ":refs/heads/nyc-*\"]\n"
+            + "  srcRepo = "
+            + manifestKey.get()
+            + "\n"
+            + "  srcRef = blablabla\n"
+            + "  srcPath = default.xml\n");
+
+    String remoteXml = "  <remote name=\"origin\" fetch=\"" + canonicalWebUrl.get() + "\" />\n";
+    String originXml = "  <default remote=\"origin\" revision=\"refs/heads/master\" />\n";
+
+    // XML change will trigger commit to superproject.
+    String xml =
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+            + "<manifest>\n"
+            + remoteXml
+            + originXml
+            + "  <project name=\""
+            + testRepoKeys[0].get()
+            + "\" path=\"project1\" />\n"
+            + "</manifest>\n";
+
+    Result src1ManifestPush =
+        pushFactory
+            .create(admin.newIdent(), manifestRepo, "Subject", "default.xml", xml)
+            .to("refs/heads/nyc-1");
+    src1ManifestPush.assertOkStatus();
+
+    xml =
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+            + "<manifest>\n"
+            + remoteXml
+            + originXml
+            + "  <project name=\""
+            + testRepoKeys[1].get()
+            + "\" path=\"project2\" />\n"
+            + "</manifest>\n";
+
+    Result src2ManifestPush =
+        pushFactory
+            .create(admin.newIdent(), manifestRepo, "Subject", "default.xml", xml)
+            .to("refs/heads/nyc-2");
+    src2ManifestPush.assertOkStatus();
+
+    BranchApi branch1 = gApi.projects().name(superKey.get()).branch("refs/heads/nyc-1");
+    assertThat(branch1.file("project1").getContentType()).isEqualTo("x-git/gitlink; charset=UTF-8");
+    assertThrows(ResourceNotFoundException.class, () -> branch1.file("project2"));
+    assertThat(branch1.file(SUPERMANIFEST_STAMP).asString())
+        .isEqualTo(
+            manifestKey.get() + " refs/heads/nyc-1 " + src1ManifestPush.getCommit().getName());
+
+    BranchApi branch2 = gApi.projects().name(superKey.get()).branch("refs/heads/nyc-2");
+    assertThat(branch2.file("project2").getContentType()).isEqualTo("x-git/gitlink; charset=UTF-8");
+    assertThrows(ResourceNotFoundException.class, () -> branch2.file("project1"));
+    assertThat(branch2.file(SUPERMANIFEST_STAMP).asString())
+        .isEqualTo(
+            manifestKey.get() + " refs/heads/nyc-2 " + src2ManifestPush.getCommit().getName());
+  }
+
+  @Test
   public void manifestIncludesOtherManifest() throws Exception {
     setupTestRepos("project");
 
@@ -658,6 +724,111 @@ public class RepoSuperManifestIT extends LightweightPluginDaemonTest {
     assertThat(branch4.file("project1").getContentType()).isEqualTo("x-git/gitlink; charset=UTF-8");
     assertThat(branch4.file(SUPERMANIFEST_STAMP).asString())
         .startsWith(manifestKey.get() + " refs/heads/src4");
+  }
+
+  @Test
+  public void overlappingRefSpecDestination() throws Exception {
+    setupTestRepos("project");
+
+    // Configure supermanifest to same superproject for main-* and *-release.
+    // An update to main-x-release triggers an overlapping write
+    pushConfig(
+        "[superproject \""
+            + superKey.get()
+            + ":refs/heads/main-*\"]\n"
+            + "  srcRepo = "
+            + manifestKey.get()
+            + "\n"
+            + "  srcRef = blablabla\n"
+            + "  srcPath = default.xml\n"
+            + "\n"
+            + "[superproject \""
+            + superKey.get()
+            + ":refs/heads/*-release\"]\n"
+            + "  srcRepo = "
+            + manifestKey.get()
+            + "\n"
+            + "  srcRef = blablabla\n"
+            + "  srcPath = default.xml\n");
+
+    String remoteXml = "  <remote name=\"origin\" fetch=\"" + canonicalWebUrl.get() + "\" />\n";
+    String originXml = "  <default remote=\"origin\" revision=\"refs/heads/master\" />\n";
+
+    String xml =
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+            + "<manifest>\n"
+            + remoteXml
+            + originXml
+            + "  <project name=\""
+            + testRepoKeys[0].get()
+            + "\" path=\"project1\" />\n"
+            + "</manifest>\n";
+
+    // This fails but silently. Check there is no destination written.
+    pushFactory
+        .create(admin.newIdent(), manifestRepo, "Subject", "default.xml", xml)
+        .to("refs/heads/main-x-release")
+        .assertOkStatus();
+
+    // This branch should not exist
+    BranchApi branch2 = gApi.projects().name(superKey.get()).branch("refs/heads/main-x-release");
+    assertThrows(ResourceNotFoundException.class, () -> branch2.file(".gitmodules"));
+
+    // Using update_manifest
+    RestResponse r =
+        adminRestSession.post(
+            "/projects/" + manifestKey + "/branches/main-x-release/update_manifest");
+    r.assertPreconditionFailed();
+  }
+
+  @Test
+  public void globsFromDifferentSources() throws Exception {
+    setupTestRepos("project");
+
+    NameKey manifest2Key = projectOperations.newProject().name(name("manifest2")).create();
+    TestRepository<InMemoryRepository> manifest2Repo = cloneProject(manifest2Key, admin);
+
+    pushConfig(
+        "[superproject \""
+            + superKey.get()
+            + ":refs/heads/main-*\"]\n"
+            + "  srcRepo = "
+            + manifestKey.get()
+            + "\n"
+            + "  srcRef = blablabla\n"
+            + "  srcPath = default.xml\n"
+            + "\n"
+            + "[superproject \""
+            + superKey.get()
+            + ":refs/heads/*-release\"]\n"
+            + "  srcRepo = "
+            + manifest2Key.get()
+            + "\n"
+            + "  srcRef = blablabla\n"
+            + "  srcPath = default.xml\n");
+
+    String remoteXml = "  <remote name=\"origin\" fetch=\"" + canonicalWebUrl.get() + "\" />\n";
+    String originXml = "  <default remote=\"origin\" revision=\"refs/heads/master\" />\n";
+
+    String xml =
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+            + "<manifest>\n"
+            + remoteXml
+            + originXml
+            + "  <project name=\""
+            + testRepoKeys[0].get()
+            + "\" path=\"project1\" />\n"
+            + "</manifest>\n";
+
+    pushFactory
+        .create(admin.newIdent(), manifest2Repo, "Subject", "default.xml", xml)
+        .to("refs/heads/main-x-release")
+        .assertOkStatus();
+
+    // The conf from manifest2 is ignored, so the push above should not create this branch
+    BranchApi secondConfBranch =
+        gApi.projects().name(superKey.get()).branch("refs/heads/main-x-release");
+    assertThrows(ResourceNotFoundException.class, () -> secondConfBranch.file(".gitmodules"));
   }
   // TODO - should add tests for all the error handling in configuration parsing?
 }
